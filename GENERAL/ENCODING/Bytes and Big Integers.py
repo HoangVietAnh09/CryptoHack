0a829654cdaa995a6b352764192d9186200f9b49
Hoang Viet Anh <vietanh092004@gmail.com>
2024-02-18 18:30:18 +0700

    write_up

--- GENERAL/ENCODING/Bytes and Big Integers.py
+++ GENERAL/ENCODING/Bytes and Big Integers.py
@@ -1,7 +1,7 @@
-from cryptography.utils import int_to_bytes
+from Crypto.Util.number import *
 
 key = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 
-key = int_to_bytes(key)
+key = long_to_bytes(key)
 
 print(key.decode())
