1e13497dfcfd05f2b3eaf946c199de33a085c272
Hoang Viet Anh <vietanh092004@gmail.com>
2024-02-19 10:02:20 +0700

    write_up

---
+++ CRYPTO ON THE WEB/No Way JOSE.py
@@ -0,0 +1,6 @@
+import jwt
+
+
+### def create_session(username):
+###     encoded = jwt.encode({'username': username, 'admin': False}, SECRET_KEY, algorithm='HS256')
+###     return {"session": encoded} 
