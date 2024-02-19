0f536830b2814dbf8a0cd1e58fbec297452dab47
Hoang Viet Anh <vietanh092004@gmail.com>
2024-02-19 11:03:21 +0700

    write_up

--- CRYPTO ON THE WEB/No Way JOSE.py
+++ CRYPTO ON THE WEB/No Way JOSE.py
@@ -1,6 +1,26 @@
 import jwt
+import base64
 
 
-### def create_session(username):
-###     encoded = jwt.encode({'username': username, 'admin': False}, SECRET_KEY, algorithm='HS256')
-###     return {"session": encoded} 
+VERIFY_SIGNATURE = 'AXeh-1tmrMyAd4iBWdri6YSJebySwkhAOAuvCWMdcFs'
+
+JOSE_header = '{"typ":"JWT","alg":"none"}'
+JOSE_bytes = JOSE_header.encode('ascii')
+base64_bytes = base64.b64encode(JOSE_bytes)
+JOSE_header_encoded = base64_bytes.decode('ascii')
+JOSE_header_encoded = JOSE_header_encoded.strip('=')
+
+
+JOSE_payload = '{"username": "admin","admin": true}'
+JOSE_bytes = JOSE_payload.encode('ascii')
+base64_bytes = base64.b64encode(JOSE_bytes)
+JOSE_payload_encodeed = base64_bytes.decode('ascii')
+JOSE_payload_encodeed = JOSE_payload_encodeed.strip('=')
+
+#In ra token voi username = admin
+print(f"{JOSE_header_encoded}.{JOSE_payload_encodeed}.{VERIFY_SIGNATURE}")
+
+
+
+
+
